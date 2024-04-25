539e0aa89b58daa301193238d538ef9c6c457987
Utarou <lkennethjan@gmail.com>
2024-04-25 16:15:28 +0800

    Updated Commit

--- signup.php
+++ signup.php
@@ -8,18 +8,17 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <script src="https://kit.fontawesome.com/f8e1898ab0.js" crossorigin="anonymous"></script>
-    <script src="script.js" defer></script>
 </head>
 <body>
     <div class="overlay">
-        <div class="signup-container">
+        <form class="signup-container" action="includes/formhandler.inc.php" method="post">
             <label class="signup-message">CREATE ACCOUNT</label>
-            <input type="text" id="email" name="email" placeholder="Enter School Email">
-            <input type="password" id="password" name="password" placeholder="Create Password">
+            <input type="text" id="email" name="create-email" placeholder="Enter School Email">
+            <input type="password" id="password" name="create-password" placeholder="Create Password">
             <input type="password" id="re-password" name="re-password" placeholder="Re-enter Password">
-            <button type="button" class="signup-button" id="signup-button">SIGN UP</button>
+            <button type="submit" class="signup-button" id="signup-button">SIGN UP</button>
             <button type="button" class="re-login" id="re-login">Back to Login</button>
-        </div>
+        </form>
     </div>
 </body>
 </html>
